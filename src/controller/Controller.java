111df494f5855e8879b6d71c5bc147b03dfdf375
pany100 <pany100@11b00e3c-3a72-8e6b-3eaf-3abda50f4fcb>
2013-10-15 00:24:13 +0000

    Kickoff

---
+++ src/controller/Controller.java
@@ -0,0 +1,19 @@
+package controller;
+
+import view.View;
+
+@SuppressWarnings("rawtypes")
+public class Controller {
+	
+	private View view;
+
+	public View getView() {
+		return view;
+	}
+
+	public void setView(View view) {
+		this.view = view;
+	}
+	
+	
+}
---
+++ src/view/View.java
@@ -0,0 +1,28 @@
+package view;
+
+import javax.swing.JFrame;
+
+import controller.Controller;
+
+public abstract class View<C extends Controller> extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -404819740792466309L;
+	private C controller;
+	
+	public C getController() {
+		return controller;
+	}
+
+	public void setController(C controller) {
+		this.controller = controller;
+	}
+
+	public void display() {
+		this.setVisible(Boolean.TRUE);
+	}
+	
+	
+}
