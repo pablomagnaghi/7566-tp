873ff6fd236f732aebd055b915ed581ce3166521
pany100 <pany100@11b00e3c-3a72-8e6b-3eaf-3abda50f4fcb>
2013-10-15 19:26:55 +0000

    Orden de tareas

--- trunk/src/controller/ProgresoController.java
+++ trunk/src/controller/ProgresoController.java
@@ -1,13 +1,75 @@
 package controller;
 
+import java.awt.Toolkit;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Random;
+
+import javax.swing.SwingWorker;
+
 import view.Progreso;
 
-public class ProgresoController extends Controller{
+public class ProgresoController extends Controller implements PropertyChangeListener{
+	
+	private Task task;
+	
+	class Task extends SwingWorker<Void, Void> {
+        /*
+         * Main task. Executed in background thread.
+         */
+        @Override
+        public Void doInBackground() {
+            Random random = new Random();
+            int progress = 0;
+            //Initialize progress property.
+            setProgress(0);
+            while (progress < 100) {
+                //Sleep for up to one second.
+                try {
+                    Thread.sleep(random.nextInt(1000));
+                } catch (InterruptedException ignore) {}
+                //Make random progress.
+                progress += random.nextInt(10);
+                setProgress(Math.min(progress, 100));
+            }
+            return null;
+        }
+ 
+        /*
+         * Executed in event dispatching thread
+         */
+        @Override
+        public void done() {
+            Toolkit.getDefaultToolkit().beep();
+            ((Progreso)getView()).notifyEndOfProgress();
+        }
+    }
 	
 	@SuppressWarnings("unchecked")
 	public ProgresoController(Progreso view){
 		this.setView(view);
 		this.getView().setController(this);
 	}
+	
+	/**
+     * Invoked when the user presses the start button.
+     */
+    public void handleButtonIniciar () {
+    	((Progreso)getView()).disableButtons();
+        task = new Task();
+        task.addPropertyChangeListener(this);
+        task.execute();
+    }
+	
+    /**
+     * Invoked when task's progress property changes.
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if ("progress" == evt.getPropertyName()) {
+            int progress = (Integer) evt.getNewValue();
+        	((Progreso)getView()).setChanges(progress, task.getProgress());
+        }
+    }
 
 }
